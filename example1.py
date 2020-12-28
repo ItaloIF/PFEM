dad858d4e2b4b2db261cacc140002417c7c0baae
Italo Inocente <iinocentef@uni.pe>
2020-12-28 15:52:36 -0500

    edit test

--- example1.py
+++ example1.py
@@ -1,6 +1,11 @@
-import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
-x = np.arange(0, 3 * np.pi, 0.1)
-y = np.sin(x)
-plt.plot(x, y)
+import numpy as np
+t = np.arange(0.0, 2.0, 0.01)
+s = 1 + np.sin(2 * np.pi * t)
+fig, ax = plt.subplots()
+ax.plot(t, s)
+ax.set(xlabel='time (s)', ylabel='voltage (mV)',
+       title='About as simple as it gets, folks')
+fig.savefig('test.png')
 plt.show()
