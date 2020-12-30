7c3afe7716f13b80abd7aa2e20fa9d197fb20af6
Italo Inocente <iinocentef@uni.pe>
2020-12-30 15:26:21 -0500

    add circ model

--- soil_1.py
+++ soil_1.py
@@ -12,26 +12,29 @@ ops.wipe()
 ops.model('basic', '-ndm', 2, '-ndf', 2)
 
 # lectura del  archivo msh
-meshName = 'gmsh/port1.msh'
+meshName = 'gmsh/mc2.msh'
   # generacion del diccionario
-nodeDic = {(0,1): 1,(0,2): 1, (0,5): 1, (0,6): 1,
-            (1,1): 1, (1,5): 1}
+nodeDic = {(0,2): 1,(0,3): 1, (0,1): 2, (0,4): 2, (0,5): 2,
+            (1,3): 1, (1,1): 2, (1,5): 2}
 eleDic = {(2,1): 1}
 Node, Ele = readMesh(meshName, nodeDic, eleDic)
 nNode = len(Node)
 nEle = len(Ele)
 
 # paraview out
-outPV('out/port_1')
+outPV('out/mc2')
 
 # definir y construir el material
 mTg = 1
-nu = 0.15 # Poisson's ratio of soil
-E = 2460000*N/cm**2
+rho = 2000*kg/m**3  # densidad del suelo
+Vs = 350.0*m/s # velocidad de inda de corte del suelo
+G = rho*Vs*Vs # modulo de corte
+nu = 0.15 # coeficiente de poisson
+E = 2*G*(1+nu)
 ops.nDMaterial('ElasticIsotropic', mTg, E, nu)
 
 # espesor de los elementos
-B = 0.25*m
+B = 1*m
 
 # construccion de nodos
 for i in range(nNode):
@@ -40,36 +43,33 @@ for i in range(nNode):
 # construccion de elementos
 for i in range(nEle):
     if (Ele[i][0] == 1):
-        ops.element('quad', i+1, *Ele[i][2:], B, 'PlaneStress', Ele[i][0])
+        ops.element('quad', i+1, *Ele[i][2:], B, 'PlaneStrain', Ele[i][0])
 
 # condiciones de frontera
 boundFix(nNode, Node)
 
 ops.timeSeries('Linear',1)
 ops.pattern('Plain',1,1)
-fx = 10*kN
-fy = 0
-ops.load(8, fx, fy)
-#for i in range(nNode):
-#    if (Node[i][0] == 2):
-#        ops.load(i+1, fx, fy)
+fx = 0
+fy = -1*kN
+for i in range(nNode):
+    if (Node[i][0] == 2):
+        ops.load(i+1, fx, fy)
 
-ops.system('FullGeneral') # probar otros solvers: 'UmfPack' 'SparseSYM'
+ops.system('FullGeneral') # probar otros solvers: 'UmfPack' 
 ops.numberer('Plain')
 ops.constraints('Plain')
-ops.integrator('LoadControl',0.1)
+ops.integrator('LoadControl',1)
 ops.algorithm('Linear')
 ops.analysis('Static')
-ops.analyze(10)
+ops.start()
+ops.analyze(1)
+ops.stop()
 
 # Grafico de la deformada
 fig = plt.figure(figsize=(10,10))
-opsv.plot_defo(200)
+opsv.plot_defo(50000)
 plt.show()
-
-# Desplazamiento
-disp = ops.nodeDisp(8,2)
-print(disp)
 
 # plot esfuerzos quad 2D
 fig = plt.figure(figsize=(10,10))
